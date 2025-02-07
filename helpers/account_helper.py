8c7632262a31737d5a9b72990fd2520d2520cdb1
egolubeva <impgolubeva@yandex.ru>
2025-02-07 17:17:44 +0300

    добавили decorator

--- helpers/account_helper.py
+++ helpers/account_helper.py
@@ -1,8 +1,26 @@
+from http.client import responses
+
 from services.dm_api_account import DMApiAccount
 from services.api_mailhog import MailHogApi
 from json import loads
 from json import JSONDecodeError
 import time
+
+def retrier(function):
+    def wrapper(*args, **kwargs):
+        token = None
+        count = 0
+        while token is None:
+            print(f"Попытка получения токена номер {count}")
+            token=function(*args, **kwargs)
+            count += 1
+            if count == 5:
+                raise AssertionError("Превышено количество попыток получения активационного токена")
+            if token:
+                return token
+            print(f"Попытка получения токена номер {count}")
+            time.sleep(1)
+    return wrapper
 
 class AccountHelper:
     def __init__(
@@ -13,30 +31,6 @@ class AccountHelper:
         self.dm_account_api = dm_account_api
         self.mailhog = mailhog
 
-    # def register_new_user(
-    #         self,
-    #         login: str,
-    #         password: str,
-    #         email: str
-    # ):
-    #
-    #     json_data = {
-    #         'login': login,
-    #         'email': email,
-    #         'password': password,
-    #     }
-    #     response = self.dm_account_api.account_api.post_v1_account(json_data=json_data)
-    #     assert response.status_code == 201, f"Пользователь не был создан, {response.json()}"
-    #     time.sleep(
-    #         0.5
-    #         )  # Введение задержки, тк иногда возникает ситуация, когда в response еще нет письма с нужным логином
-    #     response = self.mailhog.mailhog_api.get_api_v2_messages()
-    #     assert response.status_code == 200, "Письма не получены"
-    #     token = self.get_activation_token_by_login(login=login, response=response)
-    #     response = self.dm_account_api.account_api.put_v1_account_token(token=token)
-    #     assert response.status_code == 200, "Пользователь не был активирован"
-    #
-    #     return response
     def register_new_user(self, login: str, password: str, email: str, max_attempts: int = 5):
         json_data = {
             'login': login,
@@ -45,27 +39,10 @@ class AccountHelper:
         }
         response = self.dm_account_api.account_api.post_v1_account(json_data=json_data)
         assert response.status_code == 201, f"Пользователь не был создан, {response.json()}"
-
-        token = None
-        delay = 0.5
-        for attempt in range(max_attempts):
-            response = self.mailhog.mailhog_api.get_api_v2_messages()
-            assert response.status_code == 200, "Письма не получены"
-
-            token = self.get_activation_token_by_login(login=login, response=response)
-            if token:
-                break
-
-            if attempt < max_attempts - 1:
-                time.sleep(delay)
-                delay *= 2  # Увеличиваем задержку с каждой попыткой
-
-        if not token:
-            raise AssertionError(f"Токен для пользователя {login} не был получен")
-
+        token= self.get_activation_token_by_login(login=login)
+        assert token is not None, f"Токен для пользователя {login}, не был получен"
         response = self.dm_account_api.account_api.put_v1_account_token(token=token)
         assert response.status_code == 200, "Пользователь не был активирован"
-
         return response
 
     def user_login(
@@ -83,28 +60,6 @@ class AccountHelper:
             assert response.status_code == 200, "Пользователь не был авторизован"
             return response
 
-    # def update_user_email(self, login:str, password:str, new_email:str):
-    #     # Смена email
-    #     json_data = {
-    #         'login': login,
-    #         'password': password,
-    #         'email': new_email,
-    #     }
-    #     response = self.dm_account_api.account_api.put_v1_account_email(json_data)
-    #     assert response.status_code == 200, "Email не был изменен"
-    #     # Попытка войти после смены почты
-    #     response = self.dm_account_api.login_api.post_v1_account_login(json_data=json_data)
-    #     assert response.status_code == 403, "Вход пользователя не был запрещен"
-    #     # Получение писем из почтового ящика
-    #     time.sleep(0.5)  # Введение задержки, тк иногда возникает ситуация, когда в response еще нет письма с нужным логином
-    #     response = self.mailhog.mailhog_api.get_api_v2_messages()
-    #     assert response.status_code == 200, "Письма не получены"
-    #     # Поиск токена для смены email
-    #     token = self.get_activation_token_by_email(email=new_email, response=response)
-    #     assert token is not None, f'токен для новой почты {new_email} пользователя {login} не был получен'
-    #     # Активация пользователя с новым email
-    #     response = self.dm_account_api.account_api.put_v1_account_token(token=token)
-    #     assert response.status_code == 200, "Пользователь не был активирован"
     def update_user_email(
             self,
             login: str,
@@ -151,20 +106,16 @@ class AccountHelper:
         # Активация пользователя с новым email
         response = self.dm_account_api.account_api.put_v1_account_token(token=token)
         assert response.status_code == 200, "Пользователь не был активирован"
-    @staticmethod
-    def get_activation_token_by_login(login,response):
+
+    @retrier
+    def get_activation_token_by_login(self,login):
+        response = self.mailhog.mailhog_api.get_api_v2_messages()
         token = None
-        for item in response.json().get('items', []):
-            try:
-                # Предполагаем, что формат в JSON, но может быть и текст
-                user_data = loads(item['Content']['Body'])
-                user_login = user_data.get('Login')
-                if user_login == login:
-                    token = user_data['ConfirmationLinkUrl'].split('/')[-1]
-                    break
-            except (JSONDecodeError, KeyError):
-                # Если не удается декодировать JSON или отсутствует ключ, переходим к следующему элементу
-                continue
+        for item in response.json()['items']:
+            user_data = loads(item['Content']['Body'])
+            user_login = user_data.get('Login')
+            if user_login == login:
+                token = user_data['ConfirmationLinkUrl'].split('/')[-1]
         return token
 
     @staticmethod
