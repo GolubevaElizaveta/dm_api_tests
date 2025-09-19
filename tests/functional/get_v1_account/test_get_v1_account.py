0d74e58fcd057acb1c3f516d50f1c27b7125fe05
egolubeva <egolubeva@ozon.ru>
2025-09-19 15:47:44 +0300

    pyhamcrest

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,6 +1,36 @@
 
+from hamcrest import (
+    assert_that,
+    has_property,
+    starts_with,
+    all_of,
+    has_properties,
+    has_items,
+    greater_than_or_equal_to
+)
+
 def test_get_v1_account_auth(auth_account_helper):
-    auth_account_helper.dm_account_api.account_api.get_v1_account(validate_response=True)
+    response = auth_account_helper.dm_account_api.account_api.get_v1_account(validate_response=True)
+    assert_that(
+        response, all_of(
+            has_property(
+                'resource', has_properties(
+                    {
+                        'login': starts_with("egolubeva"),
+                        'roles': has_items("Guest", "Player"),
+                        'rating': has_properties(
+                            {
+                                "quality": greater_than_or_equal_to(0),
+                                "quantity": greater_than_or_equal_to(0)
+                            }
+                        )
+                    }
+                )
+            )
+        )
+    )
+    print(response)
+
 
 
 def test_get_v1_account_no_auth(account_helper):
