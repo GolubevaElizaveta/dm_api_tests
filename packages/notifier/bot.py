5c33f7df48c923e7fd7e1d33b969f7f68150b34a
impgolubeva <impgolubeva@yandex.ru>
2025-10-05 16:27:53 +0300

    telegram

--- packages/notifier/bot.py
+++ packages/notifier/bot.py
@@ -5,14 +5,14 @@ from telebot import TeleBot
 from telegram_notifier.exceptions import TelegramNotifierError
 from vyper import v
 
-config = Path(__file__).parent.joinpath('../../').joinpath('config')
+config = Path(__file__).parent.parent.parent.joinpath('config')
 v.set_config_name("prod")
 v.add_config_path(config)
 v.read_in_config()
 
 def send_file() -> None:
     telegram_bot = TeleBot(v.get("telegram.token"))
-    file_path = Path(__file__).parent.joinpath('../../').joinpath("swagger-coverage-dm-api-account.html")
+    file_path = Path(__file__).parent.parent.parent.parent.joinpath("swagger-coverage-dm-api-account.html")
     with open(file_path, 'rb') as document:
         telegram_bot.send_document(
             v.get("telegram.chat_id"),
